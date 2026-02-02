0083390792e895b82076297171ed8253898ca301
lucidrains <lucidrains@gmail.com>
2026-02-01 19:18:53 -0800

    they used an ema copy of the model as the teacher

--- sdft_pytorch/sdft_pytorch.py
+++ sdft_pytorch/sdft_pytorch.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
+from typing import Callable
 
 import torch
 from torch import nn, Tensor
 from torch.nn import Module
 
 from einops import rearrange
-from torch_einops_utils import pack_with_inverse
+
+from ema_pytorch import EMA
 
 # helpers
 
@@ -19,7 +21,16 @@ def default(v, d):
 
 class SDFT(Module):
     def __init__(
-        self
+        self,
+        model: Module,
+        ema_alpha = 0.01
     ):
         super().__init__()
+
+        self.student = model
+        self.teacher = EMA(model, include_online_model = False)
+
+    def forward(
+        self
+    ):
         raise NotImplementedError
--- tests/test_sdft.py
+++ tests/test_sdft.py
@@ -2,4 +2,15 @@ import torch
 from sdft_pytorch import SDFT
 
 def test_sdft():
-    assert True
+    from x_transformers import TransformerWrapper, Decoder
+
+    model = TransformerWrapper(
+        num_tokens = 256,
+        max_seq_len = 512,
+        attn_layers = Decoder(
+            dim = 512,
+            depth = 2
+        )
+    )
+
+    sdft_wrapper = SDFT(model)
